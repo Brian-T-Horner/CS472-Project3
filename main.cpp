f742d16c862761fb8bc30309ff89551001db2cca
Brian Horner <bhorner@suffolk.edu>
2022-04-26 20:06:46 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// CS472
+//
+// Author: Brian Horner
+// Edit History:
+// 4/22/2022 - Initial Version
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
